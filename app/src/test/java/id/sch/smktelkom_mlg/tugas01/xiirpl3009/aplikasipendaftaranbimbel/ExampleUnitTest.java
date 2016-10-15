f35ca56f32a513c3a0c77ae9e127a7f28eb0ddc8
Alvian <alviandwi888@gmail.com>
2016-10-15 21:04:52 +0700

    Pembuatan Design

---
+++ app/src/test/java/id/sch/smktelkom_mlg/tugas01/xiirpl3009/aplikasipendaftaranbimbel/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package id.sch.smktelkom_mlg.tugas01.xiirpl3009.aplikasipendaftaranbimbel;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
